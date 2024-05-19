b8462d85b37eb826b32a6b23be41d963d2d926aa
Solomon Adzape <adzapesolomon@gmail.com>
2024-05-19 23:42:18 +0100

    Added state with useReducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import Header from "./Header";
 import Main from "./Main";
+import { useEffect, useReducer } from "react";
 
+const initialState = {
+  questions: [],
+  //status can be 'loading', 'error', 'ready' 'active' or 'finished'
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataError":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action unknown");
+  }
+}
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataError" }));
+  });
   return (
     <div className="app">
       <Header />
