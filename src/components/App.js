274e32396371a7bcaa20c26aabb9769964da0d0f
Solomon Adzape <adzapesolomon@gmail.com>
2024-05-21 06:44:32 +0100

    Added question answer and progress functionality

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import { useEffect, useReducer } from "react";
 import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
   questions: [],
@@ -12,6 +14,7 @@ const initialState = {
   status: "loading",
   index: 0,
   answer: null,
+  points: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -26,17 +29,28 @@ function reducer(state, action) {
     case "start":
       return { ...state, status: "active" };
     case "newAnswer":
-      return { ...state, answer: action.payload };
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return { ...state, answer: null, index: state.index + 1 };
     default:
       throw new Error("Action unknown");
   }
 }
 export default function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
   const numQuestions = questions.length;
+  const maxPoints = questions.reduce((acc, cur) => acc + cur.points, 0);
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -54,11 +68,21 @@ export default function App() {
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
         {status === "active" && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Progress
+              answer={answer}
+              points={points}
+              numQuestions={numQuestions}
+              index={index}
+              maxPoints={maxPoints}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
---
+++ src/components/Progress.js
@@ -0,0 +1,15 @@
+function Progress({ answer, points, numQuestions, maxPoints, index }) {
+  return (
+    <header className="progress">
+      <progress max={numQuestions} value={index + Number(answer !== null)} />
+      <p>
+        Question <strong>{index + 1}</strong>/{numQuestions}
+      </p>
+      <p>
+        <strong>{points}</strong>/{maxPoints}
+      </p>
+    </header>
+  );
+}
+
+export default Progress;
